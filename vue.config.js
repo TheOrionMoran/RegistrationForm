8f0c806163a634297cb64771267977241fdf2d3e
TheOrionMoran <thetomasselden@gmail.com>
2022-04-05 22:21:22 +0400

    1

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/RegistrationForm/'
+  publicPath: '/RegistrationForm/',
+  configureWebpack: {
+    devtool: 'source-map'
+  }
 })
